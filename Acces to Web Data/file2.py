7a57fd8c6281e94aa71cd683b981171015aee1e7
Daulet <daulet.maksut01@gmail.com>
2018-09-09 06:52:35 +0600

    Web application

--- Acces to Web Data/file2.py
+++ Acces to Web Data/file2.py
@@ -1,3 +1,14 @@
 import re
-x = "there should be aby text 23 give some numbers 42"
-y = re.findall('')
+import socket
+#Just trained
+x = "there should be aby text 23 give some numbers 42 213 8827312"
+y = re.findall('[0-9]+', x)
+print (y)
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+print(~8)
+num = [0] * 2
+num[0] = 2
+num[1] = 3
+print(num)
