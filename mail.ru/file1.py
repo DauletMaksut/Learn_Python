02089a439817383fd3886414212bd5a4ba4b17ad
Daulet <daulet.maksut01@gmail.com>
2018-09-21 15:42:59 +0600

    Some changes

--- mail.ru/file1.py
+++ mail.ru/file1.py
@@ -1,8 +1,12 @@
 print("Fuck this shit")
 t= 10
-for x in range(t):
-    print(x)
-    for y in range(t):
-        print(y 
-               + 10 
-               + x)
+# for x in range(t):
+#     print(x)
+#     for y in range(t):
+#         print(y 
+#                + 10 
+#                + x)
+try:
+    print(10 / 0)
+except:
+    print("WAAA")
