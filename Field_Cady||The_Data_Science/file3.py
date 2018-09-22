0784aa268483707a00e9320c1b00a388424c9994
Daulet <daulet.maksut01@gmail.com>
2018-09-22 17:53:19 +0600

    beautiful iris histogram

--- Field_Cady||The_Data_Science/file3.py
+++ Field_Cady||The_Data_Science/file3.py
@@ -10,13 +10,14 @@ def get_iris_df():
     df['species'] = [code_species_map[c] for c in ds['target']]
     return df
 df = get_iris_df()
+rgb = (0.1, .1, .1)
 for spec in df['species'].unique():
     for_spec = df[df['species'] == spec]
-    for_spec['petal length (cm)'].plot(kind='hist', alpha=0.8,subplots=True, label = spec)
+    for_spec['petal length (cm)'].plot(kind='hist', alpha=0.6,subplots=True, label = spec, edgecolor='black', color=rgb )
+    tmp = list(rgb)
+    tmp[0] += .3
+    rgb = tuple(tmp)
 plt.legend(loc='upper right')
 plt.suptitle('Should be overlapping')
 plt.savefig('iris_hist_1.jpg')
-# df.plot(kind='hist', subplots=True, layout=(2,2))
-# plt.suptitle('Histogram for iris data')
-# plt.show()
 
