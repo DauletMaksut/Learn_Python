5ec4655f9b98a131b92800331dac0034210631fd
Daulet <daulet.maksut01@gmail.com>
2018-09-28 21:09:04 +0600

    made basic maze

--- Maze/task1.py
+++ Maze/task1.py
@@ -54,9 +54,9 @@ def we_are_cool(N_pool, p):
 
 
 N = int(input("Enter number (N): "))
-p = int(input("Enter number (p): "))
 K = int(input("Enter number (K): "))
 k = int(input("Enter number (k): "))
+p = int(input("Enter number (p): "))
 
 K_pool = [node(x) for x in range(K)]
 N_pool = [node(x) for x in range(K, N)]
@@ -69,77 +69,37 @@ for K_element in K_pool:
         tracker += 1
         if tracker == len(N_pool):
             tracker = 0
+if we_are_cool(K_pool, k):
+    print("Hard to do")
+    quit()         
 if level_one(N_pool, p):
     print("Sorry i cannot do that")
     quit()
-end = tracker
-for node in N_pool:
-    for x in N_pool:
-        if len(N_pool) == end:
-            end = tracker
-            break
-        if node.get_len() == p:
-            print('exit,',node.get_id(),node.get_con())
-            break
-        if node.get_id() == N_pool[end].get_id():
-            continue
-        if N_pool[end].get_len() == p:
-            continue
-        node.add_edge(N_pool[end].get_id())
-        N_pool[end].add_edge(node.get_id())
-        end += 1
-    print("mnb")
-    # for r_node in N_pool:
-    #     if node.get_len() == p:
-    #         break
-    #     if r_node.get_len() == p:
-    #         continue
-    #     if r_node.get_id() == node.get_id():
-    #         continue 
-    #     if node.get_id() in r_node.get_con():
-    #         continue
-    #     node.add_edge(r_node.get_id())
-    #     r_node.add_edge(node.get_id())
 
-        
+for node in N_pool:
+    if node.get_len() == p:
+        continue
+    for x in range(len(N_pool)):
+        print("node", node.get_id())
+        if node.get_len() == p:
+            print("Made node", node.get_id())
+            break
+        if node.get_id() == N_pool[tracker].get_id():
+            tracker += 1
+            continue
+        if N_pool[tracker].get_len() == p:
+            tracker += 1
+            continue
+        node.add_edge(N_pool[tracker].get_id())
+        N_pool[tracker].add_edge(node.get_id())
+        tracker += 1
+        print("tracker", tracker, node.get_id())
+        if tracker == len(N_pool):
+            tracker = 0
+    print("Made node in for", node.get_id(), node.get_con())
+
+  
 if not we_are_cool:
     print("It is sad to say but sry")
     quit()
-
-    # for i in range(len(N_pool)):
-    #     for y in range(len(N_pool)):
-    #         max_node -= 1
-    #         if max_node == -1:
-    #             max_node = len(N_pool) - 1
-    #         if N_pool[max_node].get_len() != p
-    #             break
-    #     for z in range(len(N_pool) - 1):
-    #         if N_pool[min_node]
-            
-
-        
-    #     if N_pool[min_node].get_id() not in N_pool[max_node].get_con():
-    #         N_pool[min_node].add_edge(N_pool[max_node].get_id())
-    #         N_pool[max_node].add_edge(N_pool[min_node].get_id())
-
-# while not we_are_cool(N_pool, p):
-#     print("Suka")
-#     current_node = N_pool[take_node]
-#     tmp = take_node
-#     for i in range(len(N_pool) - 1):
-#         tmp+=1
-#         change_len = current_node.get_len()
-#         if tmp == len(N_pool):
-#             tmp = 0
-#         if N_pool[tmp].get_len != p:
-#             current_node.add_edge(N_pool[tmp].get_id())
-#             N_pool[tmp].add_edge(current_node.get_id())
-#             if current_node.get_len != change_len:
-#                 break
-#     take_node += 1
-#     if take_node == len(N_pool):
-#         take_node = 0
-    # if take_node == tracker:
-    #     print("Emm, but nio nio nio i won't do that")
-    #     quit()
 output(K_pool, N_pool)
